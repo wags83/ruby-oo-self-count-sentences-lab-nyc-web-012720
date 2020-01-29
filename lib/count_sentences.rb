cc7e7c7858c1dca8cc1f174a4dbe64223890fc2f
wags83 <gpwagner@gmail.com>
2020-01-29 18:17:50 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
+#   def count_sentences
+#     counter = 0
+#       self.chars.each do |char|
+#         if char == "." || char == "?" ||char == "!"
+#         counter += 1
+#         end
+#       end
+#       counter
+#   end
+# end
 
-  end
+def count_sentences
+  self.scan(/[^\.!?]+[\.!?]/).count
+end
 end
